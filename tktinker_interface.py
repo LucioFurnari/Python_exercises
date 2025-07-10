295c6c6682d7913808792c2f3a717dae9abf9408
LucioFurnari <luciofurnari99@gmail.com>
2025-07-10 19:31:13 -0300

    feat: using input and label

--- tktinker_interface.py
+++ tktinker_interface.py
@@ -6,8 +6,18 @@ root.title("Test window")
 
 root.config(background="#57bdff")
 
-newFrame = Frame()
+newFrame = Frame(root, width=650, height=450)
 newFrame.pack()
-newFrame.config(width=650, height=450)
 newFrame.config(background="#ff8c57")
+
+newLabel = Label(newFrame, text="Hello, this is a label in python", background="#ff8c57")
+newLabel.place(x=100,y=200)
+newLabel.grid(row=1, column=1)
+
+nameLabel = Label(newFrame, text="Name:")
+nameLabel.grid(row=0, column=0)
+
+inputName = Entry(newFrame)
+inputName.grid(row=0, column=1)
+
 root.mainloop()
