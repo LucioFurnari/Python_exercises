6e58848ac891319170d63a1019df6c173c345524
LucioFurnari <luciofurnari99@gmail.com>
2025-07-15 15:54:08 -0300

    feat: add inputs

--- CRUD app/crud_interface.py
+++ CRUD app/crud_interface.py
@@ -27,4 +27,28 @@ crudMenu.add_command(label="Delete")
 
 menuNav.add_cascade(label="Crud", menu=crudMenu)
 
+
+#------------- Inputs frame -------------#
+inputsFrame = Frame(root)
+inputsFrame.pack()
+
+idInput = Entry(inputsFrame)
+idInput.grid(row=0, column=1, padx=10, pady=10)
+nameInput = Entry(inputsFrame)
+nameInput.grid(row=1, column=1, padx=10, pady=10)
+passwordInput = Entry(inputsFrame)
+passwordInput.grid(row=2, column=1, padx=10, pady=10)
+passwordInput.config(show="*") # Show * instead of text
+lastnameInput = Entry(inputsFrame)
+lastnameInput.grid(row=3, column=1, padx=10, pady=10)
+directionInput = Entry(inputsFrame)
+directionInput.grid(row=4, column=1, padx=10, pady=10)
+
+commentaryText = Text(inputsFrame, width=16, height=5)
+commentaryText.grid(row=5, column=1)
+scrollVert = Scrollbar(inputsFrame, command=commentaryText.yview)
+scrollVert.grid(row=5, column=2, sticky="nsew")
+
+commentaryText.config(yscrollcommand=scrollVert.set)
+
 root.mainloop()
