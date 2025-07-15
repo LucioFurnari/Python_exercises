cbc2b9b2af228df661b2b4a7b60728130754ee44
LucioFurnari <luciofurnari99@gmail.com>
2025-07-14 22:49:19 -0300

    feat: add multi insert

--- example_DB.py
+++ example_DB.py
@@ -6,6 +6,15 @@ db = sqlite3.connect("FirstDB")
 cursor = db.cursor()
 # cursor.execute("CREATE TABLE PRODUCTS (Article_name VARCHAR(50), Price INTEGER, Section VARCHAR(20) )")
 # cursor.execute("INSERT INTO PRODUCTS VALUES('Ball', 15, 'Sport')")
+
+variousProducts = [
+  ("Shirt", 10, "Sport"),
+  ("Jar", 90, "Ceramic"),
+  ("Toy Truck", 20, "Toy")
+]
+
+# cursor.executemany("INSERT INTO PRODUCTS VALUES (?,?,?)", variousProducts)
+
 db.commit()
 ## Close db
 db.close()
