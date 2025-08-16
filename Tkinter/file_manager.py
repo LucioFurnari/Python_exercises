ea9102164081cfc4ebe5f255b9fa5bd22800f7c8
LucioFurnari <luciofurnari99@gmail.com>
2025-08-16 16:12:06 -0300

    feat: add function for show files and selected

--- Tkinter/file_manager.py
+++ Tkinter/file_manager.py
@@ -14,8 +14,15 @@ def select_folder():
 
   show_files(path)
 
+def show_selection(listbox):
+  selection = listbox.curselection()
+  if selection:
+    index = selection[0]
+    file = listbox.get(index)
+    print(f"You selected: {file}")
+
 def show_files(path):
-  files = os.listdir(path)
+  directory = os.listdir(path)
 
   list = tk.Listbox(window)
   scrollbar = tk.Scrollbar(window)
@@ -26,9 +33,14 @@ def show_files(path):
   list.pack(side="left", fill="both", expand=True)
   scrollbar.pack(side="right", fill="y")
 
-  for file in files:
+  for file in directory:
     name, extension = os.path.splitext(file)
-    list.insert(tk.END, f"{name}{extension}")
+    file_path = os.path.join(path, file)
+
+    if os.path.isfile(file_path):
+      print(file)
+      list.insert(tk.END, f"{name}{extension}")
+      list.bind("<<ListboxSelect>>", lambda e: show_selection(list))
 
 
 select_folder_label = tk.Label(window, text="Select folder")
